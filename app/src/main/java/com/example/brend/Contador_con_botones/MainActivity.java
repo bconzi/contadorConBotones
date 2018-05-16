f371c168b2b9265e2cab3f27514dd5708b3ca6d7
Brenda Conzi <[brendatests@gmail.com]>
2018-05-16 01:16:39 -0300

    Subiendo contador con botones opción negativo

--- app/src/main/java/com/example/brend/Contador_con_botones/MainActivity.java
+++ app/src/main/java/com/example/brend/Contador_con_botones/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.brend.Contador_con_botones;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -10,38 +12,55 @@ public class MainActivity extends Activity {
 
     //variable que me cuenta la cantidad de veces que se pulsa el botón
     public int contador;
+    public TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textoResultado= (TextView)findViewById(R.id.numero);
         contador=0;
     }
 
     public void incrementaContador(View view){
 
-    contador ++;
-
-    mostrarResultado();
+        contador ++;
+        textoResultado.setText(""+contador);
+    //mostrarResultado(); comentado en video 18
     }
 
     public void decrementaContador(View view){
 
         contador --;
-        mostrarResultado();
+
+        if (contador<0){
+
+            CheckBox negativos=(CheckBox) findViewById(R.id.negativos);
+
+            if (!negativos.isChecked()){
+
+                contador=0;
+            }
+        }
+        textoResultado.setText(""+contador);
     }
+
     public void reseteaContador(View view){
 
-        contador=0;
-        mostrarResultado();
+        EditText numero_reset= (EditText) findViewById(R.id.n_reseteo);
+
+
+        try {
+            contador = Integer.parseInt(numero_reset.getText().toString());
+        } catch (Exception e){
+            contador=0;
+        }
+        numero_reset.setText("");
+
+        textoResultado.setText(""+contador);
     }
 
-    //Cambiar el texto de la variable contador
-    public void mostrarResultado(){
 
-       TextView textoResultado= (TextView) findViewById(R.id.contador_pulsaciones);
-
-       textoResultado.setText("Contador: "+contador);
-    }
 }
